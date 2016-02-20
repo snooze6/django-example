b174fce1734eb0fc45ba4da3d78d0e70774acae2
Snooze <armandonogueirar@gmail.com>
2016-02-20 23:54:01 +0000

    Login funcionando

--- users/views.py
+++ users/views.py
@@ -1,11 +1,32 @@
+# coding=utf-8
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout as lojout
+from django.contrib.auth import logout as lojout, authenticate, login as lojin
+
 
 # Create your views here.
 
 
 def login(request):
-    pass
+    error_messages=[]
+    if request.method == 'POST':
+        # # Esto peta cuando cambiamos el name de l formulario
+        # username = request.POST['usr']
+        # password = request.POST['pwd']
+        username = request.POST.get('usr')
+        password = request.POST.get('pwd')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            error_messages.append("Nombre de usuario o contraseña incorrectos")
+        else:
+            if user.is_active:
+                lojin(request, user)
+                return redirect('photos_home')
+            else:
+                error_messages.append("El usuario no está activo")
+    context = {
+        'errors': error_messages
+    }
+    return render(request, 'users/login.html', context)
 
 
 def logout(request):
