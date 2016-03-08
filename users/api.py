72237dd643d399cfc7621ef532ddc6b0901caed3
Snooze <armandonogueirar@gmail.com>
2016-03-08 17:22:06 +0000

    Anadida la API de borrado

--- users/api.py
+++ users/api.py
@@ -43,3 +43,8 @@ class UserDetailAPI(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, var):
+        user = get_object_or_404(User, pk=var)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
