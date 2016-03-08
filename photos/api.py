ce6c9e8764259106fd0773e36afb3a137554954e
Snooze <armandonogueirar@gmail.com>
2016-03-08 17:58:14 +0000

    Cambiados los parametros que devuelve cada peticion

--- photos/api.py
+++ photos/api.py
@@ -1,12 +1,15 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from photos.models import Photo
-from photos.serializers import PhotoSerializer
+from photos.serializers import PhotoSerializer, PhotoListSerializer
 
 
 class PhotoListAPI(ListCreateAPIView):
     queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
+    serializer_class = PhotoListSerializer
+
+    def get_serializer_class(self):
+        return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
 
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
--- photos/serializers.py
+++ photos/serializers.py
@@ -6,3 +6,7 @@ from photos.models import Photo
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
+
+class PhotoListSerializer(PhotoSerializer):
+    class Meta(PhotoSerializer.Meta):
+        fields = ('id', 'name', 'url')
